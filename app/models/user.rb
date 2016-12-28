31d1b3f983ddb176cf170c65afcb36439de45356
nguyen.thai.phuong@framgia.com <nguyen.thai.phuong@framgia.com>
2016-12-28 17:16:21 +0700

    add list user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,19 @@ class User < ApplicationRecord
   has_many :suggest_products, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  mount_uploader :avatar, PictureUploader
+
   validates :name, presence: true, length: {maximum: Settings.maximum.name}
-  validates :address, presence: true, 
+  validates :address, presence: true,
     length: {maximum: Settings.maximum.address}
   validates :phone, presence: true, length: {maximum: Settings.maximum.phone}
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, presence: true, length: {maximum: Settings.maximum.email},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, 
+  validates :password, presence: true,
     length: {maximum: Settings.maximum.password}, allow_nil: true
 
   before_save {email.downcase!}
@@ -24,5 +26,9 @@ class User < ApplicationRecord
 
   def is_user? user
     self == user
+  end
+
+  scope :by_name, ->name do
+    where "name LIKE '%#{name}%'" if name.present?
   end
 end
